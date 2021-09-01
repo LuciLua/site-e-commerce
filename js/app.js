ecb1e4fce4d7a25ab187d9314520d963d80657eb
LuciLua <luci.lua81@gmail.com>
2021-09-01 14:24:42 -0300

    :star2: width and height img right menu

--- js/app.js
+++ js/app.js
@@ -6,3 +6,17 @@ const menu = ()=>{
     var menul = document.querySelector('.ul-l')
     menul.classList.toggle('ul-l-x')
 }
+
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
